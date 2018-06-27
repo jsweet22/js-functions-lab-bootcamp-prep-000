c9fe9eaa0325b43eb43b4b56c56b5c6ccd8cd322
Janae Sweet <janaemarie_@hotmail.com>
2018-06-27 04:08:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // write your code below!
+function happyHolidays() {
+console.log('Happy holidays!')
+}
+happyHolidays
+
+function happyHolidaysTo(name) {
+console.log(`Happy holidays, ${name}!`)
+}
+happyHolidaysTo("you")
+
+function happyHolidayTo() {
+console.log(`Happy ${holiday}, ${name}!`)
+}
+happyHolidayTo
+
+function holidayCountdown() {
+console.log(`It's ${days} days until ${holiday}!`)
+}
+holidayCountdown
